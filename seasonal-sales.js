44f23f82cb90f7e4a808dbdade045f75db68cd0e
David Dassau <todieahero@hotmail.com>
2017-08-28 17:33:18 -0500

    added select tags

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,28 +1,42 @@
 
 var mainContainerDiv = document.getElementById("mainSSContainer");
-var jsonProductArray = [];
-var jsonDepartmentsArray = [];
-var count = 0;
+var jsonProductArray = []; // empty array for products objects
+var jsonDepartmentsArray = []; //empty array for departments/categories objects 
+var count = 0; //counter, set to zero, to count through each array
 
+// function to count...
 function counter(){
 	count++
 	if (count === 2) {
-		console.log("You got 2");
+		domProducts();
+		console.log(jsonDepartmentsArray,jsonProductArray);
 	}
 }
 
 
-function domString(){
-	var productString = "";
-	for(var i = 0; i < products.length; i++){
-		productString += `<div class="mainProductDiv">`;
-		productString +=   `<h2 class="productName">${products[i].name}</h2>`;
-		productString +=   `<h3 class="departmentName">${departments[i].id}</h3>`;
-		productString +=   `<div class="productPrice">${products[i].price}</div>`; 
-		productString += `</div>`;
-	} 
-	writeToDom(productString);
-}
+
+
+function domProducts() {
+    let productString = ''; 
+    jsonProductArray.forEach(function(index){
+            productString += `<div id='productCards'>`;
+            productString +=     `<h2>${index.name}</h2>`; 
+            if (index.category_id === 1) {
+            productString +=     `<h3>${jsonDepartmentsArray[0].name}</h3>`;
+            } else if (index.category_id === 2) {
+                productString +=     `<h3>${jsonDepartmentsArray[1].name}</h3>`;    
+            } else if (index.category_id === 3) {
+                productString +=     `<h3>${jsonDepartmentsArray[2].name}</h3>`;    
+            }
+            productString +=     `<h4>${index.price}</h4>`;
+            productString += `</div>`;      
+    });             
+    writeToDom(productString);
+};
+
+
+
+
 
 // function to print to dom
 function writeToDom(productString){
@@ -39,7 +53,7 @@ function productsJSONLoad(){
 // function to ...
 function departmentsJSONLoad(){
 	var data = JSON.parse(this.responseText);
-	jsonDepartmentsArray = data.departments;
+	jsonDepartmentsArray = data.categories;
 	counter();
 }
 
@@ -63,9 +77,9 @@ myRequest.send();
 
 
 //
-var myRequest = new XMLHttpRequest();
-myRequest.addEventListener("load", departmentsJSONLoad);
-// myRequest.addEventListener("error", departmentsJSONError);
-myRequest.open("GET", "departments.json");
-myRequest.send();
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", departmentsJSONLoad);
+// myRequest2.addEventListener("error", departmentsJSONError);
+myRequest2.open("GET", "departments.json");
+myRequest2.send();
 
